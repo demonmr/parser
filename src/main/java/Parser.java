13486e8efcf2a0d1e8ce6bb3a07762d190127904
Dmitriy <demonmr02@gmail.com>
2020-10-20 10:47:48 +0300

    testing task update connections

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -11,11 +11,15 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.CapabilityType;
 import java.io.IOException;
 import java.io.StringReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Parser {
     private List<Product> listElements;
+    private WebDriver driver;
     private int maxSize = 50;
     public Parser() {
         this.listElements = new ArrayList<>();
@@ -29,13 +33,17 @@ public class Parser {
                 return new NtlmPasswordAuthentication("corp.local", "miroshnitchenko", "titaniuM-1996");
             }
         });
+
         ChromeOptions options = new ChromeOptions();
-        Proxy proxy = new Proxy();
-        proxy.setHttpProxy("10.1.6.16:8080");
-        options.setCapability("proxy", proxy);
+        if (!netIsAvailable()) {
+            Proxy proxy = new Proxy();
+            proxy.setHttpProxy("10.1.6.16:8080");
+            options.setCapability("proxy", proxy);
+        }
         options.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
-        System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver(options);
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        driver = new ChromeDriver(options);
+
         while (maxSize <= 200) {
             driver.get("https://gpsfront.aliexpress.com/getRecommendingResults.do?callback=jQuery183022083401740659658_1603098902021&widget_id=5547572&platform=pc&limit=50&offset=" + maxSize + "&phase=1&productIds2Top=&postback=0cd58a2a-5c63-4d4f-b969-b70c6f0af63d&_=1603098964896");
 
@@ -55,6 +63,20 @@ public class Parser {
         driver.close();
 
         return listElements;
+    }
+
+    private static boolean netIsAvailable() {
+        try {
+            final URL url = new URL("https://www.google.com");
+            final URLConnection conn = url.openConnection();
+            conn.connect();
+            conn.getInputStream().close();
+            return true;
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            return false;
+        }
     }
 
 
